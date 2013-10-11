2a052a6f0ca1cbdf032d2e213dbec5c001629c7e
Alexander Clelland <Argonanth@gmail.com>
2013-10-11 14:49:28 -0400

    Ruote Prototype Repository Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RuotePrototype::Application.config.secret_key_base = '4aac7c3b11876f766bb468b4297a92636118ea8be52a07983e28d2f7a45bcae8357471433753609f83a533744fae3fd818f97c90f14d0e5018fa7b0806c39f55'
