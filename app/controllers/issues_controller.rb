5a62c7b31b87899a76f7f21ddff571a1a1c59609
Alexander Clelland <Argonanth@gmail.com>
2013-10-12 22:04:51 -0400

    Started issue creation

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,10 +1,19 @@
 class IssuesController < ApplicationController
 
     def index
-
-        render 'index'
+        # display the view that shows the input fields required to create an issue
     end
 
+    def create
+        #create the issue
+        #start the work participants for the current issue
+            #note sure what they will be actually doing though...
 
+        render text: 'BLAH'
+    end
+
+    def destroy
+        #close the issue
+    end
 
 end
